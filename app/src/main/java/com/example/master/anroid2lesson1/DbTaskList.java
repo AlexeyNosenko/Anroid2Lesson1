789af90cf13e7ed3f7771d5d79398c9f9df97fe7
Nosenko Alexey <onyx_2009@li.ru>
2017-01-20 14:53:44 +0300

    Minimal functional

--- app/src/main/java/com/example/master/anroid2lesson1/DbTaskList.java
+++ app/src/main/java/com/example/master/anroid2lesson1/DbTaskList.java
@@ -2,10 +2,13 @@ package com.example.master.anroid2lesson1;
 
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.SyncAdapterType;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
+import android.util.Log;
+
+import static com.example.master.anroid2lesson1.DbTaskList.TblTasks.CLN_REF_MAIN_TASK;
+import static com.example.master.anroid2lesson1.DbTaskList.TblTasks.TBL_NAME;
 
 /**
  * Created by Master on 19.01.2017.
@@ -15,6 +18,8 @@ public class DbTaskList extends DBHelper {
 
     public static final String DB_NAME = "Task_list";
     public static final int DB_VERSION = 1;
+
+    public static final int EMPTY_MAIN_TASK = -1;
 
     public static class TblTasks implements BaseColumns {
 
@@ -32,19 +37,42 @@ public class DbTaskList extends DBHelper {
                         + CLN_REF_MAIN_TASK + " integer,"
                         + "FOREIGN KEY(" + CLN_REF_MAIN_TASK + ") REFERENCES Tasks(" + _ID + ")"
                         + ");";
+
     }
 
-    public Cursor getAllData() {
+    public Cursor getAllTasks() {
         return this.getWritableDatabase().query(TblTasks.TBL_NAME, null, null, null, null, null, null);
     }
 
     public long addTask(String task){
+        return addTask(task, EMPTY_MAIN_TASK);
+    }
+
+    public long addTask(String task, long mainTaskId){
         ContentValues contentValues = new ContentValues();
         contentValues.put(TblTasks.CLN_TASK, task);
+        if (mainTaskId != EMPTY_MAIN_TASK) {
+            contentValues.put(TblTasks.CLN_REF_MAIN_TASK, mainTaskId);
+        }
         return this.getWritableDatabase().insert(TblTasks.TBL_NAME, null, contentValues);
     }
 
+    public Cursor getTasks(){
+        return getTasks(EMPTY_MAIN_TASK);
+    }
+
+    public Cursor getTasks(long mainTask){
+        String where = mainTask == EMPTY_MAIN_TASK ? CLN_REF_MAIN_TASK + " is null " : CLN_REF_MAIN_TASK + " = " + mainTask;
+//        Log.d("TaskList", "getTasks: " + where);
+        return this.getWritableDatabase().query(TblTasks.TBL_NAME, null, where, null, null, null, null);
+    }
+
+    public long deleteSubTasks(long main_id){
+        return this.getWritableDatabase().delete(TBL_NAME, TblTasks.CLN_REF_MAIN_TASK + " = " + main_id, null);
+    }
+
     public long deleteTask(long id){
+        deleteSubTasks(id);
         return this.getWritableDatabase().delete(TblTasks.TBL_NAME, TblTasks._ID + " = " + id, null);
     }
 
@@ -54,6 +82,7 @@ public class DbTaskList extends DBHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        Log.d("TaskList", "onCreate: Create DB");
         DBHelper.execSQL(db, TblTasks.SQL_CREATE_DB);
     }
 
--- app/src/main/java/com/example/master/anroid2lesson1/MainActivity.java
+++ app/src/main/java/com/example/master/anroid2lesson1/MainActivity.java
@@ -3,21 +3,19 @@ package com.example.master.anroid2lesson1;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
-import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Switch;
+import android.widget.SimpleCursorAdapter;
+
+import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,41 +25,73 @@ public class MainActivity extends AppCompatActivity {
     private final int MENU_LIST_DELETE = 2;
 
     private final String DL_TITLE = "Enter task";
-    private final String DL_MESSAGE = "Enter task";
+    private final String DL_MESSAGE = "";
     private final String DL_BTN_POSITIVE = "Ok";
     private final String DL_BTN_NEGATIVE = "Cancel";
+
+    public final String LOG_TAG = "TaskList";
 
     private Cursor cursor;
 
     private DbTaskList dbTaskList;
+
+    private Stack<Long> transitionTask = new Stack<>();
+    private long currentTask = DbTaskList.EMPTY_MAIN_TASK;
+
+    @Override
+    public void onBackPressed() {
+        if (transitionTask.empty()) {
+            super.onBackPressed();
+        } else {
+            long task = transitionTask.pop();
+            currentTask = task;
+            Log.d(LOG_TAG, "onBackPressed " + task);
+            getTasks(task);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lvList = (ListView) findViewById(R.id.lvList);
+        lvList = (ListView) findViewById(R.id.lvListTask);
+        lvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(LOG_TAG, "onItemListClick: " + currentTask);
+                transitionTask.push(currentTask);
+                currentTask = id;
+                getTasks(id);
+            }
+        });
+
         registerForContextMenu(lvList);
 
         dbTaskList = new DbTaskList(this);
+        getTasks(DbTaskList.EMPTY_MAIN_TASK);
+    }
 
-        cursor = dbTaskList.getAllData();
+    private void getTasks(long task_id){
+        cursor = dbTaskList.getTasks(task_id);
         startManagingCursor(cursor);
+//        Log.d(LOG_TAG, "cursor.getCount(): " + cursor.getCount());
+        updateList();
+    }
 
-        String[] column = new String[]{//DbTaskList.TblTasks._ID,
-                                       DbTaskList.TblTasks.CLN_TASK
-                                       //DbTaskList.TblTasks.CLN_COMPLETE,
-                                       //DbTaskList.TblTasks.CLN_REF_MAIN_TASK
-                                      };
-
+    private void updateList(){
+        cursor.requery();
+        String[] header = new String[]{ DbTaskList.TblTasks.CLN_TASK };
+        SimpleCursorAdapter userAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, header, new int[] {android.R.id.text1}, 0);
+        lvList.setAdapter(userAdapter);
     }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         switch (v.getId()){
-            case R.id.lvList:
-                menu.add(0, MENU_LIST_ADD, 0, getResources().getString(R.string.menu_add));
-                menu.add(0, MENU_LIST_DELETE, 0, getResources().getString(R.string.menu_delete));
+            case R.id.lvListTask:
+                menu.add(0, MENU_LIST_ADD, 0, getResources().getString(R.string.menu_context_add));
+                menu.add(0, MENU_LIST_DELETE, 0, getResources().getString(R.string.menu_context_delete));
                 break;
         }
     }
@@ -70,17 +100,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onContextItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case MENU_LIST_ADD:
-                addData();
+                addRowInList(((AdapterView.AdapterContextMenuInfo) item.getMenuInfo()).id);
                 break;
 
             case MENU_LIST_DELETE:
-                AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-                dbTaskList.deleteTask(menuInfo.id);
-
+                deleteRowInList(((AdapterView.AdapterContextMenuInfo) item.getMenuInfo()).id);
                 break;
         }
         return super.onContextItemSelected(item);
     }
+
+    private void deleteRowInList(long id) {
+        dbTaskList.deleteTask(id);
+        updateList();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -98,13 +132,14 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_add) {
-            addData();
+            addRowInList(currentTask);
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void addData() {
+    private void addRowInList(final long id) {
+        Log.d(LOG_TAG, "addRowInList: " + id);
         AlertDialog.Builder dl = new AlertDialog.Builder(this);
         dl.setTitle(DL_TITLE);
         dl.setMessage(DL_MESSAGE);
@@ -116,15 +151,24 @@ public class MainActivity extends AppCompatActivity {
         dl.setPositiveButton(DL_BTN_POSITIVE, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                dbTaskList.addTask(newTask.getText().toString());
+                Log.d(LOG_TAG, "onClickPositive: " + newTask.getText().toString());
+                dbTaskList.addTask(newTask.getText().toString(), id);
+                updateList();
             }
         });
 
         dl.setNegativeButton(DL_BTN_NEGATIVE, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                Log.d(LOG_TAG, "onClickNegative");
             }
         });
+        dl.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        dbTaskList.close();
+        super.onDestroy();
     }
 }
